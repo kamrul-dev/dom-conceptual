e81acc230c3dd2b80cf0d3999a6ed83a1edf5173
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-12 14:50:08 +0600

    counting using direct function for optimization

--- js/app.js
+++ js/app.js
@@ -1,15 +1,41 @@
 
-// add button event handler
+/* // add button event handler
 let count = 0;
-document.getElementById('add').addEventListener('click', function(){
+document.getElementById('add').addEventListener('click', function () {
     count = count + 1;
     document.getElementById('count').innerText = count;
 });
 
 // minus button event handler
-document.getElementById('minus').addEventListener('click', function(){
-    count = count -1;
-    if(count >= 0){
+document.getElementById('minus').addEventListener('click', function () {
+    if (count > 0) {
+        count = count - 1;
         document.getElementById('count').innerText = count;
     }
-});
+    else (
+        window.alert('Number should be positive')
+    )
+}); */
+
+
+// counting using direct function 
+
+let count = 0;
+function add() {
+    count++;
+    control('count');
+}
+
+function minus() {
+    if (count > 0) {
+        count--;
+        control('count');
+    }
+    else (
+        window.alert('Number should be positive')
+    )
+}
+
+function control(id) {
+    document.getElementById(id).innerText = count;
+}
