89e38c2cbcead3a88254b0d989e7ef73262d0e0a
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-12 14:02:34 +0600

    minus counting done with negative number validation

--- js/app.js
+++ js/app.js
@@ -5,3 +5,11 @@ document.getElementById('add').addEventListener('click', function(){
     count = count + 1;
     document.getElementById('count').innerText = count;
 });
+
+// minus button event handler
+document.getElementById('minus').addEventListener('click', function(){
+    count = count -1;
+    if(count >= 0){
+        document.getElementById('count').innerText = count;
+    }
+});
